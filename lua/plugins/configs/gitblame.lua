4a78c029ce28840b8258c7f771b45ad790048d94
minhman2501 <phminhman2501@gmail.com>
2025-02-03 18:37:17 +0700

    Add nvim surround and change message template

--- lua/plugins/configs/gitblame.lua
+++ lua/plugins/configs/gitblame.lua
@@ -1,10 +1,9 @@
 local opts = {
   -- your configuration comes here
-  -- for example
-  enabled = true, -- if you want to enable the plugin
-  message_template = " <author>, <date> • <summary> • <<sha>>", -- template for the blame message, check the Message template section for more options
-  date_format = "%m-%d-%Y %H:%M", -- template for the date, check Date format section for more options
-  virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+  enabled = true,
+  message_template = "<author> • <date> • <summary> • <<sha>>",
+  date_format = "%B, %d-%Y %H:%M",
+  virtual_text_column = 1,
 }
 
 return opts
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -84,4 +84,13 @@ return {
     "christoomey/vim-tmux-navigator",
     lazy = false,
   },
+
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {}
+    end,
+  },
 }
