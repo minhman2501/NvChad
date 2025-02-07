942605f86e3f47b484d0a855680dcb5d2ed0af8c
minhman2501 <phminhman2501@gmail.com>
2025-02-07 09:36:51 +0700

    Change gitblame format

--- lua/plugins/configs/gitblame.lua
+++ lua/plugins/configs/gitblame.lua
@@ -2,7 +2,7 @@ local opts = {
   -- your configuration comes here
   enabled = true,
   message_template = "<author> • <date> • <summary> • <<sha>>",
-  date_format = "%B, %d-%Y %H:%M",
+  date_format = "%r • %b %d, %Y %H:%M",
   virtual_text_column = 1,
 }
 
